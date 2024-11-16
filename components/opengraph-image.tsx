df3d13e522fa9beb34714a70e0505a37cfd19112
wr1159 <chuweiron18@gmail.com>
2024-11-16 17:14:50 +0800

    Update opengraph Update opengraph

---
+++ components/opengraph-image.tsx
@@ -0,0 +1,72 @@
+{
+    /* <div
+    style={{
+        display: "flex",
+        height: "100%",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        letterSpacing: "-.02em",
+        fontWeight: 700,
+        background: "#130D06",
+    }}
+>
+    <div
+        style={{
+            left: 42,
+            top: 42,
+            position: "absolute",
+            display: "flex",
+            alignItems: "center",
+            color: "#FF7216",
+        }}
+    >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-shopping-basket"
+        >
+            <path d="m15 11-1 9" />
+            <path d="m19 11-4-7" />
+            <path d="M2 11h20" />
+            <path d="m3.5 11 1.6 7.4a2 2 0 0 0 2 1.6h9.8a2 2 0 0 0 2-1.6l1.7-7.4" />
+            <path d="M4.5 15.5h15" />
+            <path d="m5 11 4-7" />
+            <path d="m9 11 1 9" />
+        </svg>
+        <span
+            style={{
+                marginLeft: 8,
+                fontSize: 20,
+            }}
+        >
+            BasketFi.xyz
+        </span>
+    </div>
+    <div
+        style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            padding: "20px 50px",
+            margin: "0 42px",
+            fontSize: 40,
+            width: "auto",
+            maxWidth: 550,
+            textAlign: "center",
+            backgroundColor: "#FF7216",
+            color: "#130D06",
+            lineHeight: 1.4,
+        }}
+    >
+        Create and manage your token basket.
+    </div>
+</div>; */
+}
